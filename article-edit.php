95c6d6257856f719877ef604cd9ca566136d01b7
Nagy Zoltán <zoli1984@gmail.hu>
2022-08-09 21:53:31 +0200

    meg csak ennyi egyenlőre

---
+++ article-edit.php
@@ -0,0 +1,71 @@
+                        <!DOCTYPE html>
+                        <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <link
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                                    rel="stylesheet">
+                                <link rel="stylesheet" href="./css/style.css">
+                                <script
+                                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+                                <title>Article-edit</title>
+
+                            </head>
+                            <body>
+                                <class="col-md-12">
+                                    <div class="row">
+                                        <h1>Blog-generator</h1>
+                                        <nav class="navbar navbar-expand-sm bg-light">
+
+                                            <div class="container-fluid">
+                                                <!-- Links -->
+                                                <ul class="navbar-nav">
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="index.php">Home</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="article.php">Article</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="article-edit.php">Article edit</a>
+                                                    </li>
+                                                    <li class="nav-item">
+                                                        <a class="nav-link" href="article-treatment.php">Article treatment</a>
+                                                    </li>
+                                                </ul>
+
+                                            </nav>
+                                            <div class="col-md-6">
+                                                <form action="xxx.php" method="post" class="articleform">
+                                                    <h2>Article-edit
+                                                    </h2>
+                                                    <label for="title_input">Title:</label>
+                                                    <input type="text" name="title_input"></br>
+                                            </br>
+                                            <label for="lead_input">Lead:</label>
+                                            <input type="text" name="lead_input"></br>
+                                    </br>
+                                    <label for="image_input">Image:</label>
+                                    <input type="text" name="image_input"></br>
+                            </br>
+                       
+
+                            <label for="keywords">Keywords:</label>
+                            <input type="text" name="keywords"></br>
+                        </br>
+
+                        <button typw="submit" form="article_form" value="Submit">Submit</button>
+                        <div class="col-md-6">
+                        <label for="content_input">Content:</label>
+                        <textarea rows="6" cols="6" name="content" form="articleform"></textarea>
+                        </div>
+                        </form>
+                        </br>
+                        </br>
+                        </div>
+                        </div>
+                        <!--col-md-12 end-->
+                        </body>
+                        </html>
--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,39 +1,39 @@
 555
 
-const gulp = require('gulp');
+var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('sass', function() {
-    return gulp.src('/scss/style.scss')
+    return gulp.src('../scss/style.scss')
         .pipe(sass())
-        .pipe(gulp.dest('/css'));
+        .pipe(gulp.dest('../css'));
 });
 
-gulp.task('pack-js', function() {
+/* /* gulp.task('pack-js', function() {
     return gulp.src([
-            //ARRAY OF JS FILES TO BE BUNDLED
-            /*   
-                      '../js/main.js' */
-        ])
-        .pipe(concat('index.min.js'))
-        .pipe(gulp.dest('../dist'));
-});
+        //ARRAY OF JS FILES TO BE BUNDLED
+        /*   
+                  '../js/main.js' 
+])
+
+.pipe(gulp.dest('../dist'));
+});  */
 
 gulp.task('pack-css', function() {
     return gulp.src([
             //ARRAY OF CSS FILES TO BE BUNDLED
 
-            '/css/style.css'
+            '../css/style.css'
         ])
         .pipe(concat('styles.min.css'))
         .pipe(gulp.dest('../dist'));
 });
 
-function defaultTask(cb) {
+/* function defaultTask(cb) {
     cb();
 }
-exports.default = defaultTask;
+exports.default = defaultTask; */
 
-gulp.task('default', gulp.series(['sass', 'pack-js', 'pack-css']));
-gulp.watch('./scss', gulp.series(['sass', 'pack-js', 'pack-css']))
+gulp.task('default', gulp.series(['sass', 'pack-css']));
+gulp.watch('../scss', gulp.series(['sass', 'pack-css']));
